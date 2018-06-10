5308ed05508e60a3c033b0bc48f8557d804ce327
violetcat <zhalia14@gmail.com>
2018-06-10 20:25:27 +0200

    hide some containers and add first input handling

--- Multiplication table scos container/test/script/etap1stylRealistyczny.js
+++ Multiplication table scos container/test/script/etap1stylRealistyczny.js
@@ -2,47 +2,53 @@
 
 $(document).ready( function() {
 		
-	var step='etap2stylRealistyczny';
+	var step;
+	
+	
 		
+	$('input').keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+	
+	$('#zad1input1').keyup(function(){
+		var input = $(this);
+		var number = input.val();
+		console.log('wpisal '+number);
 		
-			
-	$("#finishButton").click(function(){
-		console.log('finish');
-		getScoByKeyword()
-		//setObjectivesStatusForSCO();
-	});
-			
-	function getScoByKeyword(){
-		
-		doInitialize();
-		console.log('nasz keyword: '+step);
-		
-		var objectivesCount = doGetValue("cmi.objectives._count");
-		var keywordOfWrong = step;
-		var recommendationRule = "KEYWORD_EQUAL_TO";
-		for (var i=0; i < objectivesCount; i++) {
-			var idObjective = doGetValue("cmi.objectives." + i + ".id");
-			if (idObjective == "its.run") {
-				
-				console.log('Jestem w its run! Pierwszy objectives');
-				console.log(doGetValue("cmi.objectives." + i + ".description"));
-				
-				var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
-				var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
-				var trigger_actions = ta1 + ";" + ta2;
-				doSetValue("cmi.objectives." + i + ".description", trigger_actions);
-				doCommit();
-				console.log('Jestem w its run! 2 objectives');
-				console.log(doGetValue("cmi.objectives." + i + ".description"));
-				break;
-			}
-			
+		if (number==8 ) {
+			input.removeClass("incorrect");
+			input.addClass("correct disabled");
+			$('#congratMessage1').removeClass('is-hidden');
+			$('#errorMessage1').addClass('is-hidden');
+			$('#zad1part2').removeClass('is-hidden');
 		}
-	}		
+		else {
+			input.addClass("incorrect");
+			$('#errorMessage1').removeClass('is-hidden');
+		}
+		
+    });
+	
 			
 });
+
 
 $(window).on("unload", function(e) {
     doTerminate();
 });
-
