5a18d479e275077f06215c13c93bdef7f3e03ccd
violetcat <zhalia14@gmail.com>
2018-06-17 16:48:27 +0200

    add function that disables sco after finishing it

--- Multiplication table course/test/script/wyborEtapu.js
+++ Multiplication table course/test/script/wyborEtapu.js
@@ -3,164 +3,201 @@
 $(document).ready( function() {
 		
 	var step;
+	var objective;
+	var a ;
+	
+	setA();
+	if (a==1) {
+		console.log('ha a a a');
+	}
+	function setA	() {
+		a=1;
+	}	
 	
 	
-		
-	$('input').keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-             // Allow: Ctrl/cmd+A
-            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+C
-            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+X
-            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
-    });
+	console.log('przed getObjectives');
+	getObjectives();
+	console.log('po getObjectives');
 	
-	$('#step1input1').keyup(function(){
-		if ($('#step1input2').val()) {
-			var input1 = $(this);
-			var input2 = $('#step1input2');
-			
-			var firstNumber = input1.val();
-			var secondNumber = input2.val();
-			
-			if ((firstNumber==2 && secondNumber==5) || (firstNumber==5 && secondNumber==2)) {
-				input1.addClass("correct disabled");
-				input2.addClass("correct disabled");
-				$('#step2').removeClass('is-hidden');
-			}
-			else {
-				step = "etap1stylCzynnosciowy";
-				input1.addClass("incorrect disabled");
-				input2.addClass("incorrect disabled");
-				$('#finishButton').removeClass('is-hidden');
-			}
-		}
-    });
-	
-	$('#step1input2').keyup(function(){
-		if ($('#step1input1').val()) {
-			var input1 = $('#step1input1');
-			var input2 = $(this);
-			
-			var firstNumber = input1.val();
-			var secondNumber = input2.val();
-			
-			if ((firstNumber==2 && secondNumber==5) || (firstNumber==5 && secondNumber==2)) {
-				input1.addClass("correct disabled");
-				input2.addClass("correct disabled");
-				$('#step2').removeClass('is-hidden');
-			}
-			else {
-				step = "etap1stylCzynnosciowy";
-				input1.addClass("incorrect disabled");
-				input2.addClass("incorrect disabled");
-				$('#finishButton').removeClass('is-hidden');
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-			}
-		}
-    });	
-	
-
-	$('#step2input1').keyup(function(){
-		if ($('#step2input2').val()) {
-			var input1 = $(this);
-			var input2 = $('#step2input2');
-			
-			var firstNumber = input1.val();
-			var secondNumber = input2.val();
-			
-			if (firstNumber==2 && secondNumber==3) {
-				step = 'etap30';
-				input1.addClass("correct disabled");
-				input2.addClass("correct disabled");
-				$('#finishButton').removeClass('is-hidden');
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-			}
-			else {
-				step = 'etap2stylCzynnosciowy';
-				input1.addClass("incorrect disabled");
-				input2.addClass("incorrect disabled");
-				$('#finishButton').removeClass('is-hidden');
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-			}
-		}
-    });
-	$('#step2input2').keyup(function(){
-		if ($('#step2input1').val()) {
-			var input1 = $('#step2input1');
-			var input2 = $(this);
-			
-			var firstNumber = input1.val();
-			var secondNumber = input2.val();
-			
-			if (firstNumber==2 && secondNumber==3) {
-				step = 'etap30';
-				input1.addClass("correct disabled");
-				input2.addClass("correct disabled");
-				$('#finishButton').removeClass('is-hidden');
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-			}
-			else {
-				step = 'etap2stylCzynnosciowy';
-				input1.addClass("incorrect disabled");
-				input2.addClass("incorrect disabled");
-				$('#finishButton').removeClass('is-hidden');
-				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-			}
-		}
-    });
-
-	
-	$("#finishButton").click(function(){
-		console.log('keyword   '+step);
-		getScoByKeyword()
-		$('#finishMessage').removeClass('is-hidden');
-		$('#activeBody').addClass('is-hidden');
-		//setObjectivesStatusForSCO();
-	});
-	
-	
-			
-			
-	function getScoByKeyword(){
+	function getObjectives() {
 		doInitialize();
-		
 		var objectivesCount = doGetValue("cmi.objectives._count");
-		var keywordOfWrong = step;
-		var recommendationRule = "KEYWORD_EQUAL_TO";
-		for (var i=0; i < objectivesCount; i++) {
+		console.log(objectivesCount.length);
+		for (var i = 0; i < objectivesCount; i++) {
+			console.log('jestem w bjectives count');
 			var idObjective = doGetValue("cmi.objectives." + i + ".id");
-			if (idObjective == "its.run") {
-				var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
-				var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
-				var trigger_actions = ta1 + ";" + ta2;
-				doSetValue("cmi.objectives." + i + ".description", trigger_actions);
-				doCommit();
-				console.log('Jestem w its run! 2 objectives');
-				console.log(doGetValue("cmi.objectives." + i + ".description"));
-				break;
+			var status = "cmi.objectives." + i + ".success_status";
+			//var scoreRaw = "cmi.objectives." + i + ".score.raw";
+			//var scoreScaled = "cmi.objectives." + i + ".score.scaled";
+			
+			if (idObjective == "stepChoosing") {
+				console.log('status passed '+ doGetValue(status));
+				console.log('status passed type '+ typeof doGetValue(status));
+				//console.log('score raw: ' + doGetValue(scoreRaw));
+				//console.log('score scaled 1 ' + doGetValue(scoreScaled));
+				objective = doGetValue(status);
+				
+				
 			}
 		}
-	}		
-			
-			
-			
-			
-			
-			
-			
-	function setObjectivesStatusForSCO() {
+	}
+	
+	if (objective=='failed') {
+		$('#disabledMessage').removeClass('is-hidden');
+		
+	}
+	else {
+		$('#activeBody').removeClass('is-hidden');
+		
+		$('input').keydown(function (e) {
+			// Allow: backspace, delete, tab, escape, enter and .
+			if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+				 // Allow: Ctrl/cmd+A
+				(e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+C
+				(e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+X
+				(e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: home, end, left, right
+				(e.keyCode >= 35 && e.keyCode <= 39)) {
+					 // let it happen, don't do anything
+					 return;
+			}
+			// Ensure that it is a number and stop the keypress
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+				e.preventDefault();
+			}
+		});
+		
+		$('#step1input1').keyup(function(){
+			if ($('#step1input2').val()) {
+				var input1 = $(this);
+				var input2 = $('#step1input2');
+				
+				var firstNumber = input1.val();
+				var secondNumber = input2.val();
+				
+				if ((firstNumber==2 && secondNumber==5) || (firstNumber==5 && secondNumber==2)) {
+					input1.addClass("correct disabled");
+					input2.addClass("correct disabled");
+					$('#step2').removeClass('is-hidden');
+				}
+				else {
+					step = "etap1stylCzynnosciowy";
+					input1.addClass("incorrect disabled");
+					input2.addClass("incorrect disabled");
+					$('#finishButton').removeClass('is-hidden');
+				}
+			}
+		});
+		
+		$('#step1input2').keyup(function(){
+			if ($('#step1input1').val()) {
+				var input1 = $('#step1input1');
+				var input2 = $(this);
+				
+				var firstNumber = input1.val();
+				var secondNumber = input2.val();
+				
+				if ((firstNumber==2 && secondNumber==5) || (firstNumber==5 && secondNumber==2)) {
+					input1.addClass("correct disabled");
+					input2.addClass("correct disabled");
+					$('#step2').removeClass('is-hidden');
+				}
+				else {
+					step = "etap1stylCzynnosciowy";
+					input1.addClass("incorrect disabled");
+					input2.addClass("incorrect disabled");
+					$('#finishButton').removeClass('is-hidden');
+					$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+				}
+			}
+		});	
+		
+
+		$('#step2input1').keyup(function(){
+			if ($('#step2input2').val()) {
+				var input1 = $(this);
+				var input2 = $('#step2input2');
+				
+				var firstNumber = input1.val();
+				var secondNumber = input2.val();
+				
+				if (firstNumber==2 && secondNumber==3) {
+					step = 'etap30';
+					input1.addClass("correct disabled");
+					input2.addClass("correct disabled");
+					$('#finishButton').removeClass('is-hidden');
+					$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+				}
+				else {
+					step = 'etap2stylCzynnosciowy';
+					input1.addClass("incorrect disabled");
+					input2.addClass("incorrect disabled");
+					$('#finishButton').removeClass('is-hidden');
+					$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+				}
+			}
+		});
+		$('#step2input2').keyup(function(){
+			if ($('#step2input1').val()) {
+				var input1 = $('#step2input1');
+				var input2 = $(this);
+				
+				var firstNumber = input1.val();
+				var secondNumber = input2.val();
+				
+				if (firstNumber==2 && secondNumber==3) {
+					step = 'etap30';
+					input1.addClass("correct disabled");
+					input2.addClass("correct disabled");
+					$('#finishButton').removeClass('is-hidden');
+					$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+				}
+				else {
+					step = 'etap2stylCzynnosciowy';
+					input1.addClass("incorrect disabled");
+					input2.addClass("incorrect disabled");
+					$('#finishButton').removeClass('is-hidden');
+					$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+				}
+			}
+		});
+
+		
+		$("#finishButton").click(function(){
+			console.log('keyword   '+step);
+			getScoByKeyword()
+			$('#finishMessage').removeClass('is-hidden');
+			$('#activeBody').addClass('is-hidden');
+			setObjectivesStatusForSCO();
+		});
+		
+		
+				
+				
+		function getScoByKeyword(){
+			doInitialize();
+			var objectivesCount = doGetValue("cmi.objectives._count");
+			var keywordOfWrong = step;
+			var recommendationRule = "KEYWORD_EQUAL_TO";
+			for (var i=0; i < objectivesCount; i++) {
+				var idObjective = doGetValue("cmi.objectives." + i + ".id");
+				if (idObjective == "its.run") {
+					var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
+					var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
+					var trigger_actions = ta1 + ";" + ta2;
+					doSetValue("cmi.objectives." + i + ".description", trigger_actions);
+					doCommit();
+					console.log('Jestem w its run! 2 objectives');
+					//console.log(doGetValue("cmi.objectives." + i + ".description"));
+					break;
+				}
+			}
+		}		
+				
+
+		function setObjectivesStatusForSCO() {
 			var objectivesCount = doGetValue("cmi.objectives._count");
 			for (var i = 0; i < objectivesCount; i++) {
 				var idObjective = doGetValue("cmi.objectives." + i + ".id");
@@ -169,21 +206,22 @@ $(document).ready( function() {
 				var scoreScaled = "cmi.objectives." + i + ".score.scaled";
 				
 				
-				if (idObjective == "learningStyle") {
+				if (idObjective == "stepChoosing") {
 					
-					doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
-					doSetValue(scoreRaw, 23);
-					doSetValue(scoreScaled, 0.25);
+					//doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
+					doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_FAILED);
+					doSetValue(scoreRaw, 0);
+					doSetValue(scoreScaled, 1);
 					
 					console.log('status passed '+ doGetValue(status));
-					console.log('score raw 23 ' + doGetValue(scoreRaw));
-					console.log('score scaled 23 ' + doGetValue(scoreScaled));
+					console.log('score raw 0 ' + doGetValue(scoreRaw));
+					console.log('score scaled 1 ' + doGetValue(scoreScaled));
 					
 				}
 			}
 		}		
 			
-	
+	}
 			
 });
 
