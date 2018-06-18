874416e7d15b02d2d0dd15cc7cf15c46ca7914bf
violetcat <zhalia14@gmail.com>
2018-06-18 02:02:18 +0200

    add going back to previous sco

--- Multiplication table scos container/test/script/etap30.js
+++ Multiplication table scos container/test/script/etap30.js
@@ -5,110 +5,119 @@ $(document).ready( function() {
 	var step;
 	var taskPart = 1;
 	var task2Part = 1;
+	var objective = getObjectiveStatus('step3part0');
 	
+	function getObjectiveStatus(objective) {
+		doInitialize();
+		var objectivesCount = doGetValue("cmi.objectives._count");
+		for (var i = 0; i < objectivesCount; i++) {
+			var idObjective = doGetValue("cmi.objectives." + i + ".id");
+			var status = "cmi.objectives." + i + ".success_status";
+			if (idObjective == objective) {
+				return doGetValue(status);
+			}
+		}
+	}
+	
+	if (objective=='failed') {
+		$('#disabledMessage').removeClass('is-hidden');
+	}
+	else {
+		$('#activeBody').removeClass('is-hidden');
 		
-	$('input').keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-             // Allow: Ctrl/cmd+A
-            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+C
-            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+X
-            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
-    });
-	
-	$("#nextButton").click(function(){
-		taskPart++;
-		var taskId = '#zad1part'+taskPart;
-		if (taskPart<6) {
-			$(taskId).removeClass('is-hidden');
-		}
-		else {
-			$('#zad2').removeClass('is-hidden');
-			$('#nextButton').addClass('is-hidden');
-		}
-		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-	});
-	
-	$('.zad2-input').keyup(function(){
-		var input = $(this);
-		var errorMessage = input.parent().next();
-		var nextTaskPart = input.parent().parent().next();
-		var number = input.val();
-		if ( number.toString()=='0') {
-			task2Part++;
-			if (task2Part<5) {
-				input.removeClass("incorrect");
-				input.addClass("correct disabled");
-				errorMessage.addClass('is-hidden');
-				nextTaskPart.removeClass('is-hidden');
+		$('input').keydown(function (e) {
+			// Allow: backspace, delete, tab, escape, enter and .
+			if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+				 // Allow: Ctrl/cmd+A
+				(e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+C
+				(e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+X
+				(e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: home, end, left, right
+				(e.keyCode >= 35 && e.keyCode <= 39)) {
+					 // let it happen, don't do anything
+					 return;
+			}
+			// Ensure that it is a number and stop the keypress
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+				e.preventDefault();
+			}
+		});
+		
+		$("#nextButton").click(function(){
+			taskPart++;
+			var taskId = '#zad1part'+taskPart;
+			if (taskPart<6) {
+				$(taskId).removeClass('is-hidden');
 			}
 			else {
-				step = 'etap31';
-				$('#congratMessage4').removeClass('is-hidden');
-				$("#finishButton").removeClass('is-hidden');
+				$('#zad2').removeClass('is-hidden');
+				$('#nextButton').addClass('is-hidden');
 			}
 			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			errorMessage.removeClass('is-hidden');
-		}
-    });
-	
-	
-	
-
-
-
-
-	
-	$("#finishButton").click(function(){
-		getScoByKeyword()
-		$('#finishMessage').removeClass('is-hidden');
-		$('#activeBody').addClass('is-hidden');
-		//setObjectivesStatusForSCO();
-	});
-	
-	
-			
-			
-	function getScoByKeyword(){
-		doInitialize();
+		});
 		
-		var objectivesCount = doGetValue("cmi.objectives._count");
-		var keywordOfWrong = step;
-		var recommendationRule = "KEYWORD_EQUAL_TO";
-		for (var i=0; i < objectivesCount; i++) {
-			var idObjective = doGetValue("cmi.objectives." + i + ".id");
-			if (idObjective == "its.run") {
-				var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
-				var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
-				var trigger_actions = ta1 + ";" + ta2;
-				doSetValue("cmi.objectives." + i + ".description", trigger_actions);
-				doCommit();
-				break;
+		$('.zad2-input').keyup(function(){
+			var input = $(this);
+			var errorMessage = input.parent().next();
+			var nextTaskPart = input.parent().parent().next();
+			var number = input.val();
+			if ( number.toString()=='0') {
+				task2Part++;
+				
+					input.removeClass("incorrect");
+					input.addClass("correct disabled");
+					errorMessage.addClass('is-hidden');
+					nextTaskPart.removeClass('is-hidden');
+				
+				if (task2Part==5) {
+					step = 'etap31';
+					setObjectiveStatus('step3part1',1);
+					$('#congratMessage4').removeClass('is-hidden');
+					$("#finishButton").removeClass('is-hidden');
+				}
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
 			}
-		}
-	}		
+			else {
+				input.addClass("incorrect");
+				errorMessage.removeClass('is-hidden');
+			}
+		});
+		
+		
+		
+		$("#finishButton").click(function(){
+			$(this).addClass('disabled');
+			getScoByKeyword()
+			$('#finishMessage').removeClass('is-hidden');
+			$('#activeBody').addClass('is-hidden');
+			setObjectiveStatus('step3part0', 0);
+		});
+		
+		
+				
+				
+		function getScoByKeyword(){
+			doInitialize();
 			
-			
-			
-			
-			
-			
-			
-	function setObjectivesStatusForSCO() {
+			var objectivesCount = doGetValue("cmi.objectives._count");
+			var keywordOfWrong = step;
+			var recommendationRule = "KEYWORD_EQUAL_TO";
+			for (var i=0; i < objectivesCount; i++) {
+				var idObjective = doGetValue("cmi.objectives." + i + ".id");
+				if (idObjective == "its.run") {
+					var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
+					var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
+					var trigger_actions = ta1 + ";" + ta2;
+					doSetValue("cmi.objectives." + i + ".description", trigger_actions);
+					doCommit();
+					break;
+				}
+			}
+		}		
+				
+		function setObjectiveStatus (objectiveName, statusPassed) {
 			var objectivesCount = doGetValue("cmi.objectives._count");
 			for (var i = 0; i < objectivesCount; i++) {
 				var idObjective = doGetValue("cmi.objectives." + i + ".id");
@@ -116,23 +125,21 @@ $(document).ready( function() {
 				var scoreRaw = "cmi.objectives." + i + ".score.raw";
 				var scoreScaled = "cmi.objectives." + i + ".score.scaled";
 				
-				
-				if (idObjective == "learningStyle") {
-					
-					doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
-					doSetValue(scoreRaw, 23);
-					doSetValue(scoreScaled, 0.25);
-					
-					console.log('status passed '+ doGetValue(status));
-					console.log('score raw 23 ' + doGetValue(scoreRaw));
-					console.log('score scaled 23 ' + doGetValue(scoreScaled));
-					
+				if (idObjective == objectiveName) {
+					doSetValue(scoreRaw, 0);
+					doSetValue(scoreScaled, 1);
+					if (statusPassed) {
+						doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
+					}
+					else{
+						doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_FAILED);
+					}
 				}
 			}
-		}		
-			
-	
-			
+		}	
+				
+		
+	}			
 });
 
 
--- Multiplication table scos container/test/script/etap31.js
+++ Multiplication table scos container/test/script/etap31.js
@@ -5,197 +5,183 @@ $(document).ready( function() {
 	var step;
 	var taskPart = 1;
 	var task2Part = 1;
+	var goBack = 0;
+	var objective = getObjectiveStatus('step3part1');
 	
+	function getObjectiveStatus(objective) {
+		doInitialize();
+		var objectivesCount = doGetValue("cmi.objectives._count");
+		for (var i = 0; i < objectivesCount; i++) {
+			var idObjective = doGetValue("cmi.objectives." + i + ".id");
+			var status = "cmi.objectives." + i + ".success_status";
+			if (idObjective == objective) {
+				return doGetValue(status);
+			}
+		}
+	}
+	
+	if (objective=='failed') {
+		$('#disabledMessage').removeClass('is-hidden');
+	}
+	else {
+		$('#activeBody').removeClass('is-hidden');
 		
-	$('input').keydown(function (e) {
-        // Allow: backspace, delete, tab, escape, enter and .
-        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-             // Allow: Ctrl/cmd+A
-            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+C
-            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: Ctrl/cmd+X
-            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-             // Allow: home, end, left, right
-            (e.keyCode >= 35 && e.keyCode <= 39)) {
-                 // let it happen, don't do anything
-                 return;
-        }
-        // Ensure that it is a number and stop the keypress
-        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-            e.preventDefault();
-        }
-    });
-	
-	$("#nextButton").click(function(){
-		taskPart++;
-		var taskId = '#zad1part'+taskPart;
-		if (taskPart<6) {
-			$(taskId).removeClass('is-hidden');
-		}
-		else {
-			$('#zad2').removeClass('is-hidden');
-			$('#nextButton').addClass('is-hidden');
-		}
-		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-	});
-	
-	$('#zad2input1').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number==7 ) {
-			input.removeClass("incorrect");
-			input.addClass("correct disabled");
-			$('#errorMessage1').addClass('is-hidden');
-			$('#zad2part2').removeClass('is-hidden');
-			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			$('#errorMessage1').removeClass('is-hidden');
-		}
-    });
-	
-	$('#zad2input2').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number==3 ) {
-			input.removeClass("incorrect");
-			input.addClass("correct disabled");
-			$('#errorMessage2').addClass('is-hidden');
-			$('#zad2part3').removeClass('is-hidden');
-			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			$('#errorMessage2').removeClass('is-hidden');
-		}
-    });	
-	
-	$('#zad2input3').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number==9 ) {
-			input.removeClass("incorrect");
-			input.addClass("correct disabled");
-			$('#errorMessage3').addClass('is-hidden');
-			$('#zad2part4').removeClass('is-hidden');
-			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			$('#errorMessage3').removeClass('is-hidden');
-		}
-    });	
-	
-	$('#zad2input4').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number==5 ) {
-			input.removeClass("incorrect");
-			input.addClass("correct disabled");
-			$('#errorMessage4').addClass('is-hidden');
-			$('#congratMessage4').removeClass('is-hidden');
-			$('#zad3').removeClass('is-hidden');
-			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			$('#errorMessage4').removeClass('is-hidden');
-		}
-    });	
-	
-	$('#zad3input1').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number.toString()=='0') {
-			console.log('hej 0');
-			step = 'etap32';
-			input.addClass("correct disabled");
-			$('#congratMessage5').removeClass('is-hidden');
-		}
-		else {
-			step = 'etap30';
-			input.addClass("incorrect disabled");
-			$('#errorMessage5').removeClass('is-hidden');
-		}
-		$("#finishButton").removeClass('is-hidden');
-		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    });
-	
-	
-	
-	
-	$('#zad2inp').keyup(function(){
-		var input = $(this);
-		var number = input.val();
-		if ( number==0 ) {
-			task2Part++;
-			if (task2Part<5) {
-				input.removeClass("incorrect");
-				input.addClass("correct disabled");
-				errorMessage.addClass('is-hidden');
-				nextTaskPart.removeClass('is-hidden');
+		$('input').keydown(function (e) {
+			// Allow: backspace, delete, tab, escape, enter and .
+			if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+				 // Allow: Ctrl/cmd+A
+				(e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+C
+				(e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: Ctrl/cmd+X
+				(e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+				 // Allow: home, end, left, right
+				(e.keyCode >= 35 && e.keyCode <= 39)) {
+					 // let it happen, don't do anything
+					 return;
+			}
+			// Ensure that it is a number and stop the keypress
+			if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+				e.preventDefault();
+			}
+		});
+		
+		$("#nextButton").click(function(){
+			taskPart++;
+			var taskId = '#zad1part'+taskPart;
+			if (taskPart<6) {
+				$(taskId).removeClass('is-hidden');
 			}
 			else {
-				step = 'etap31';
-				
-				
+				$('#zad2').removeClass('is-hidden');
+				$('#nextButton').addClass('is-hidden');
 			}
-			
-			
 			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-		}
-		else {
-			input.addClass("incorrect");
-			errorMessage.removeClass('is-hidden');
-		}
-    });
-
-	
-	
-	
-
-
-
-
-	
-	$("#finishButton").click(function(){
-		getScoByKeyword()
-		$('#finishMessage').removeClass('is-hidden');
-		$('#activeBody').addClass('is-hidden');
-		//setObjectivesStatusForSCO();
-	});
-	
-	
-			
-			
-	function getScoByKeyword(){
-		doInitialize();
+		});
 		
-		var objectivesCount = doGetValue("cmi.objectives._count");
-		var keywordOfWrong = step;
-		var recommendationRule = "KEYWORD_EQUAL_TO";
-		for (var i=0; i < objectivesCount; i++) {
-			var idObjective = doGetValue("cmi.objectives." + i + ".id");
-			if (idObjective == "its.run") {
-				var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
-				var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
-				var trigger_actions = ta1 + ";" + ta2;
-				doSetValue("cmi.objectives." + i + ".description", trigger_actions);
-				doCommit();
-				break;
+		$('#zad2input1').keyup(function(){
+			var input = $(this);
+			var number = input.val();
+			if ( number==7 ) {
+				input.removeClass("incorrect");
+				input.addClass("correct disabled");
+				$('#errorMessage1').addClass('is-hidden');
+				$('#zad2part2').removeClass('is-hidden');
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
 			}
-		}
-	}		
+			else {
+				input.addClass("incorrect");
+				$('#errorMessage1').removeClass('is-hidden');
+			}
+		});
+		
+		$('#zad2input2').keyup(function(){
+			var input = $(this);
+			var number = input.val();
+			if ( number==3 ) {
+				input.removeClass("incorrect");
+				input.addClass("correct disabled");
+				$('#errorMessage2').addClass('is-hidden');
+				$('#zad2part3').removeClass('is-hidden');
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+			}
+			else {
+				input.addClass("incorrect");
+				$('#errorMessage2').removeClass('is-hidden');
+			}
+		});	
+		
+		$('#zad2input3').keyup(function(){
+			var input = $(this);
+			var number = input.val();
+			if ( number==9 ) {
+				input.removeClass("incorrect");
+				input.addClass("correct disabled");
+				$('#errorMessage3').addClass('is-hidden');
+				$('#zad2part4').removeClass('is-hidden');
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+			}
+			else {
+				input.addClass("incorrect");
+				$('#errorMessage3').removeClass('is-hidden');
+			}
+		});	
+		
+		$('#zad2input4').keyup(function(){
+			var input = $(this);
+			var number = input.val();
+			if ( number==5 ) {
+				input.removeClass("incorrect");
+				input.addClass("correct disabled");
+				$('#errorMessage4').addClass('is-hidden');
+				$('#congratMessage4').removeClass('is-hidden');
+				$('#zad3').removeClass('is-hidden');
+				$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+			}
+			else {
+				input.addClass("incorrect");
+				$('#errorMessage4').removeClass('is-hidden');
+			}
+		});	
+		
+		$('#zad3input1').keyup(function(){
+			var input = $(this);
+			var number = input.val();
+			if ( number.toString()=='0') {
+				step = 'etap32';
+				setObjectiveStatus('step3part2',1);
+				input.addClass("correct disabled");
+				$('#congratMessage5').removeClass('is-hidden');
+			}
+			else {
+				//step = 'etap30';
+				console.log('jestem w cofaniu sie');
+				goBack = 1;
+				setObjectiveStatus('step3part0', 1);
+				console.log('etap 3 0: '+getObjectiveStatus('step3part0'));
+				input.addClass("incorrect disabled");
+				$('#errorMessage5').removeClass('is-hidden'); //wiadomosc musisz cofnac sie do 0
+			}
+			$("#finishButton").removeClass('is-hidden');
+			$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+		});
+		
+		
+		$("#finishButton").click(function(){
+			$(this).addClass('disabled');
+			//jesli sie cofa do jednego z poprzednich sco, to nie ma co go zaciagac po keywordzie, bo ono juz tam jest
+			if (!goBack) {
+				getScoByKeyword()
+			}
+			$('#finishMessage').removeClass('is-hidden');
+			$('#activeBody').addClass('is-hidden');
+			setObjectiveStatus('step3part1', 0);
+		});
+		
+		
+				
+				
+		function getScoByKeyword(){
+			doInitialize();
 			
-			
-			
-			
-			
-			
-			
-	function setObjectivesStatusForSCO() {
+			var objectivesCount = doGetValue("cmi.objectives._count");
+			var keywordOfWrong = step;
+			var recommendationRule = "KEYWORD_EQUAL_TO";
+			for (var i=0; i < objectivesCount; i++) {
+				var idObjective = doGetValue("cmi.objectives." + i + ".id");
+				if (idObjective == "its.run") {
+					var ta1 = "\"SearchConditions=(\"general/keyword=" + keywordOfWrong + "\")\"";
+					var ta2 = "\"RecommendationRule="+ recommendationRule +"\"";
+					var trigger_actions = ta1 + ";" + ta2;
+					doSetValue("cmi.objectives." + i + ".description", trigger_actions);
+					doCommit();
+					break;
+				}
+			}
+		}		
+				
+				
+		function setObjectiveStatus (objectiveName, statusPassed) {
 			var objectivesCount = doGetValue("cmi.objectives._count");
 			for (var i = 0; i < objectivesCount; i++) {
 				var idObjective = doGetValue("cmi.objectives." + i + ".id");
@@ -203,23 +189,21 @@ $(document).ready( function() {
 				var scoreRaw = "cmi.objectives." + i + ".score.raw";
 				var scoreScaled = "cmi.objectives." + i + ".score.scaled";
 				
-				
-				if (idObjective == "learningStyle") {
-					
-					doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
-					doSetValue(scoreRaw, 23);
-					doSetValue(scoreScaled, 0.25);
-					
-					console.log('status passed '+ doGetValue(status));
-					console.log('score raw 23 ' + doGetValue(scoreRaw));
-					console.log('score scaled 23 ' + doGetValue(scoreScaled));
-					
+				if (idObjective == objectiveName) {
+					doSetValue(scoreRaw, 0);
+					doSetValue(scoreScaled, 1);
+					if (statusPassed) {
+						doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_PASSED);
+					}
+					else{
+						doSetValue(status, CMI_OBJECTIVES_SUCCESS_STATUS_FAILED);
+					}
 				}
 			}
-		}		
-			
-	
-			
+		}	
+				
+		
+	}		
 });
 
 
